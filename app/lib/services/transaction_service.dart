e51c73e82ac5fce5ff2d346554e34c7cd57362ed
Rafael Cerqueira <81481716+rafahcerqueira@users.noreply.github.com>
2024-08-14 21:56:36 -0300

    fix: Corrige erros de prints para uso de loggers (#29)

--- app/lib/services/transaction_service.dart
+++ app/lib/services/transaction_service.dart
@@ -1,7 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
 
 class TransactionService {
+  final Logger _logger = Logger('TransactionService');
+
+  TransactionService() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      _logger.log(record.level,
+          '${record.level.name}: ${record.time}: ${record.message}');
+    });
+  }
+
   Future<void> registerTransaction(Map<String, dynamic> goalData) async {
     var url = Uri.parse('http://10.0.2.2:3000/transaction');
 
@@ -15,13 +26,14 @@ class TransactionService {
       );
 
       if (response.statusCode == 201) {
-        print('Transação cadastrada com sucesso!');
+        _logger.info('Transação cadastrada com sucesso!');
       } else {
-        print('Falha ao cadastrar transação. Status: ${response.statusCode}');
-        print('Mensagem de erro: ${response.body}');
+        _logger.warning(
+            'Falha ao cadastrar transação. Status: ${response.statusCode}');
+        _logger.warning('Mensagem de erro: ${response.body}');
       }
     } catch (e) {
-      print('Erro ao fazer requisição: $e');
+      _logger.severe('Erro ao fazer requisição: $e');
     }
   }
 }
