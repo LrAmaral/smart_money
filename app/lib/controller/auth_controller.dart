bfc0804f9d2067170be0fce55a23634109e4c92d
Daniel Toledo <dan992965@gmail.com>
2024-08-22 22:47:28 -0300

    feat: adiciona userId no GetX

--- app/lib/controller/auth_controller.dart
+++ app/lib/controller/auth_controller.dart
@@ -1,15 +1,23 @@
 import 'package:get/get.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class AuthController extends GetxController {
   var accessToken = ''.obs;
+  var userId = ''.obs;
   var userProfile = <String, dynamic>{}.obs;
 
   void setAccessToken(String token) {
     accessToken.value = token;
+    setUserId(token);
   }
 
   String getAccessToken() {
     return accessToken.value;
+  }
+
+  void setUserId(String token) {
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+    userId.value = decodedToken['sub'];
   }
 
   void setUserProfile(Map<String, dynamic> profile) {
