d00577e2827b8bfe6f978505355b1ff2369a4feb
Rafael Cerqueira Gomes <rafahcerqueira@gmail.com>
2024-08-25 13:37:58 -0300

    fix: corrige bug de numero grande em transacoes e remove warnings de register e edit profile

--- app/lib/constants/env.dart
+++ app/lib/constants/env.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:3000';
+  static const String baseUrl = 'http://192.168.0.8:3000';
   static const String contentType = 'application/json';
 }
